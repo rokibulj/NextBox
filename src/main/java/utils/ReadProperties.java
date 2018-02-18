bb8d08e90131eeaa657411282f300f86afdbbf05
rokibulj <rokibulj2@gmail.com>
2018-02-17 23:53:30 -0500

    code add on project

--- src/main/java/utils/ReadProperties.java
+++ src/main/java/utils/ReadProperties.java
@@ -1,5 +1,46 @@
 package utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
 public class ReadProperties {
+
+	public String getProperty(String key) {
+		String value = null;
+		Properties prop = loadProperty("testRun");
+		value = prop.getProperty(key);
+		if (value == null) {
+			String currentEnv = prop.getProperty("testEnv");
+			prop = loadProperty(currentEnv);
+			value = prop.getProperty(key);
+		}
+		return value;
+	}
+	
+	public String getSaucelabsProperty(String key){
+		String value = null;
+		Properties prop = loadProperty("saucelabs");
+		value = prop.getProperty(key);
+		
+		return value;
+	}
+
+	private Properties loadProperty(String propFileName) {
+		Properties prop = new Properties();
+		String filePath = "./src/main/resources/properties/" + propFileName + ".properties";
+
+		try {
+			File file = new File(filePath);
+			FileInputStream stream = new FileInputStream(file);
+			prop.load(stream);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return prop;
+	}
 
 }
